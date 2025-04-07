fffa358cbdb80129746103b1fab066e7f7aad42c
David Kariuki <kinyanjuidavidkariuki@gmail.com>
2025-04-07 13:26:30 +0300

    Add files via upload

--- retrieveComment.php
+++ retrieveComment.php
@@ -2,7 +2,7 @@
 include("config.php");
 $data = json_decode(file_get_contents('php://input'), true);
 $name = $data['name'];
-$stmt = $db->query("SELECT * FROM comment where iconName = '$name' ORDER BY timestamp DESC;");
+$stmt = $db->query("SELECT * FROM comment where iconName = '$name' ORDER BY timestamp ASC;");
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
     foreach ($result as &$row) {
         $row = array_map('trim', $row);
